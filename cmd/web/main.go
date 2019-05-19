cc33605688d67b898fffbdac5e02bc35c3af9f05
Carlos Landeras <carlos.landeras@gmail.com>
2019-05-19 23:11:04 +0200

    Add tls certificate and listen with https

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -63,7 +63,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
