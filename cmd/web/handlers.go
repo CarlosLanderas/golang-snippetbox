f20535b5b9aa333cd8d397baa6274ac9f6809c05
Carlos Landeras <carlos.landeras@gmail.com>
2019-05-19 19:48:00 +0200

    Add create snippet template validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -81,7 +81,10 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if len(errors) > 0 {
-		fmt.Fprint(w, errors)
+		app.render(w, r, "create.page.tmpl", &templateData{
+			FormErrors: errors,
+			FormData:   r.PostForm,
+		})
 		return
 	}
 
