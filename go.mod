e3dd780d945a66a019fabe5dbcc2cfb097bc024f
Carlos Landeras <carlos.landeras@gmail.com>
2019-05-27 07:24:29 +0200

    Finish signup process

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/golangcollege/sessions v1.1.0
 	github.com/justinas/alice v0.0.0-20171023064455-03f45bd4b7da
+	golang.org/x/crypto v0.0.0-20181009213950-7c1a557ab941
 )
--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -3,6 +3,10 @@ package mysql
 import (
 	"database/sql"
 	"snippetbox/pkg/models"
+	"strings"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserModel struct {
@@ -10,7 +14,24 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
-	return nil
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+	VALUES( ?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			if mysqlErr.Number == 1062 && strings.Contains(mysqlErr.Message, "users_uc_email") {
+				return models.ErrDuplicateEmail
+			}
+		}
+	}
+
+	return err
+
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
